326f33bfeef1764d331189c244ada5ba6ad74d3b
danailstratirv <stratyd@abv.bg>
2019-01-30 01:39:04 +0200

    Traffic Light

---
+++ Csharp Advanced/01. Stacks and Queues/Lab 6 Traffic Light/TrafficLight.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lab_6_Traffic_Light
+{
+    class TrafficLight
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var queue = new Queue<string>();
+            int counter = 0;
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (queue.Count > 0)
+                        {
+                            Console.WriteLine($"{queue.Dequeue()} has passed!");
+                            counter++;
+                        }
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
