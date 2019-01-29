b3fd0cd3439f8464139711fbc3e62e6fecda03c1
danailstratirv <stratyd@abv.bg>
2019-01-30 01:36:48 +0200

    Matching Brackets

---
+++ Csharp Advanced/01. Stacks and Queues/Lab 4 Matching Brackets/MatchingBrackets.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lab_4_Matching_Brackets
+{
+    class MatchingBrackets
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var expressionFinder = new Stack<int>(input.Length);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    expressionFinder.Push(i);
+                }
+
+                if (input[i] == ')')
+                {
+                    int start = expressionFinder.Pop();
+                    Console.WriteLine(input.Substring(start, i - start + 1));
+                }
+            }
+        }
+    }
+}
