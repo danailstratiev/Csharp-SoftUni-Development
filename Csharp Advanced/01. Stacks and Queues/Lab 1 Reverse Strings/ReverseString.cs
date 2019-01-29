b276df7b36f34e52b65e6082aa567bf6b7eb1f3a
danailstratirv <stratyd@abv.bg>
2019-01-29 03:12:30 +0200

    Reverse Strings

---
+++ Csharp Advanced/01. Stacks and Queues/Lab 1 Reverse Strings/ReverseString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lab_1_Reverse_Strings
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stringReverser = new Stack<char>();
+
+            string input = Console.ReadLine();
+
+            foreach (var symbol in input)
+            {
+                stringReverser.Push(symbol);
+            }
+
+            while (stringReverser.Count > 0)
+            {
+                Console.Write(stringReverser.Pop());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
