f9a6e435a36e9079a0364d991d4cf591a7d21607
danailstratirv <stratyd@abv.bg>
2019-01-30 01:37:31 +0200

    Hot Potato

---
+++ Csharp Advanced/01. Stacks and Queues/Lab 5 Hot Potato/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lab_5_Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var children = Console.ReadLine().Split(' ');
+            int number = int.Parse(Console.ReadLine());
+
+            Queue<string> queue = new Queue<string>(children);
+
+            while (queue.Count != 1)
+            {
+                for (int i = 1; i < number; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
