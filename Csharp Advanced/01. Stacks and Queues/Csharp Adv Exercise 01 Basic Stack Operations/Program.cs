1147901e082658ad99fa735466bb6bb241fb2983
danailstratirv <stratyd@abv.bg>
2019-01-31 01:55:19 +0200

    Basic Stack Operations

---
+++ Csharp Advanced/01. Stacks and Queues/Csharp Adv Exercise 01 Basic Stack Operations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Csharp_Adv_Exercise_01_Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conditions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var stackLength = conditions[0];
+            var elementsToPop = conditions[1];
+            var numberToFind = conditions[2];
+            var numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < stackLength; i++)
+            {
+                stack.Push(numbers[i]);
+            }
+
+            for (int i = 0; i < elementsToPop; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            if (stack.Contains(numberToFind))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
