98d3b69181b03df12c8795600f2403522213d6ac
danailstratirv <stratyd@abv.bg>
2019-02-01 04:16:02 +0200

    Fashion Boutique

---
+++ Csharp Advanced/01. Stacks and Queues/Csharp Adv Exercise 05 Fashion Boutique/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Csharp_Adv_Exercise_05_Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clothes = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var rackCapacity = int.Parse(Console.ReadLine());
+            var stack = new Stack<int>(clothes);
+            var sum = 0;
+            var counter = 1;
+
+            while (true)
+            {
+                if (stack.Count == 0)
+                {
+                    break;
+                }                
+
+                if (sum + stack.Peek() <= rackCapacity)
+                {
+                  sum += stack.Pop();
+                }
+                else
+                {
+                    sum = 0;
+                    counter++;
+                }                              
+            }
+
+            Console.WriteLine(counter);
+
+        }
+    }
+}
