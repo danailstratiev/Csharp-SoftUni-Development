fe9ab534971eb13b69bee24a692cd2a6ef3c21e7
danailstratirv <stratyd@abv.bg>
2019-02-03 01:48:22 +0200

    Poisonous Plants

---
+++ Csharp Advanced/01. Stacks and Queues/Exercise 10 Poisonous Plants/PoisonousPlants.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Exercise_10_Poisonous_Plants
+{
+    class PoisonousPlants
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var plants = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var deadPlants = new List<int>();
+            var days = 0;
+
+            while (true)
+            {
+                for (int i = 0; i < plants.Count-1; i++)
+                {
+                    if (plants[i] < plants[i+1])
+                    {
+                        deadPlants.Add(i + 1);
+                    }
+                }
+
+                if (deadPlants.Count == 0)
+                {
+                    break;
+                }
+
+                for (int i = deadPlants.Count - 1; i >= 0; i--)
+                {
+                    plants.RemoveAt(deadPlants[i]);
+                }
+
+                days++;
+                deadPlants.Clear();
+            }
+
+            Console.WriteLine(days);
+        }
+    }
+}
