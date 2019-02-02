f64a6376bc01b125d1f8475192b38841f2d24ed3
danailstratirv <stratyd@abv.bg>
2019-02-02 17:25:15 +0200

    Stack Fibonacci

---
+++ Csharp Advanced/01. Stacks and Queues/Exercise 8 Stack Fibonacci/StackFibonacci.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Exercise_8_Stack_Fibonacci
+{
+    class StackFibonacci
+    {
+        static void Main(string[] args)
+        {
+            var n = long.Parse(Console.ReadLine());
+            var fibonacciStack = new Stack<long>();
+
+            long firstNum = 0;
+            long firstNum1 = 1;
+
+            fibonacciStack.Push(firstNum);
+            fibonacciStack.Push(firstNum1);
+
+            for (int i = 0; i < n-2; i++)
+            {
+                firstNum = firstNum1;
+                firstNum1 = fibonacciStack.Sum();
+
+                fibonacciStack.Pop();
+                fibonacciStack.Pop();
+
+                fibonacciStack.Push(firstNum);
+                fibonacciStack.Push(firstNum1);
+            }
+
+            Console.WriteLine(fibonacciStack.Sum());
+
+        }
+    }
+}
