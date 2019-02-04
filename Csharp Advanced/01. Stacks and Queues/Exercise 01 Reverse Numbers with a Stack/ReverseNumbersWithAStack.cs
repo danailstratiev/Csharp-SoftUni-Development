dcc1d183842f02f5eb2870c0d54c0dff1d7e884b
danailstratirv <stratyd@abv.bg>
2019-02-04 12:34:55 +0200

    Reverse Numbers With A Stack

---
+++ Csharp Advanced/01. Stacks and Queues/Exercise 01 Reverse Numbers with a Stack/ReverseNumbersWithAStack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Exercise_01_Reverse_Numbers_with_a_Stack
+{
+    class ReverseNumbersWithAStack
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            var stack = new Stack<int>(numbers);
+
+            while (stack.Count > 0)
+            {
+                Console.Write($"{stack.Pop()} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
