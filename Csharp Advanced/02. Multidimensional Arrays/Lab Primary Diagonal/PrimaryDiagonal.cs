a19368abd3bc7334070b5e0a62e9fa31a84305e1
danailstratirv <stratyd@abv.bg>
2019-02-05 22:55:41 +0200

    Primary Diagonal

---
+++ Csharp Advanced/02. Multidimensional Arrays/Lab Primary Diagonal/PrimaryDiagonal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Lab_Primary_Diagonal
+{
+    class PrimaryDiagonal
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var sumOfDiagonal = 0;
+
+            var matrix = new int[n, n];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var elements = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = elements[j];
+
+                    if (i == j)
+                    {
+                        sumOfDiagonal += matrix[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine(sumOfDiagonal);
+        }
+    }
+}
