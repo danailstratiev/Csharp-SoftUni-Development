edd5a635a20e844d05795882b74f290884c1c0c1
danailstratirv <stratyd@abv.bg>
2019-02-05 22:53:01 +0200

    Square With Max Sum

---
+++ Csharp Advanced/02. Multidimensional Arrays/Lab Square with Maximum Sum/SquareWithMaxSum.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Lab_Square_with_Maximum_Sum
+{
+    class SquareWithMaxSum
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix =
+            {
+                {7, 1, 3, 3, 2, 1},
+                {1, 3, 9, 8, 5, 6},
+                {4, 6, 7, 9, 1, 0}
+            };
+
+            int maxSum = int.MinValue;
+            int rowIndex = 0;
+            int colIndex = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    int sum = matrix[row, col] + matrix[row, col + 1] +
+                        matrix[row + 1, col] + matrix[row + 1, col + 1];
+
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        rowIndex = row;
+                        colIndex = col;
+                    }
+                }
+            }
+
+            for (int row = rowIndex; row < rowIndex + 2; row++)
+            {
+                for (int col = colIndex; col < colIndex + 2; col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(maxSum);
+        }
+    }
+}
