65fd69a37eaa09cbf045fd4784afb80b945404c5
danailstratirv <stratyd@abv.bg>
2019-02-05 22:51:46 +0200

    Sum Matrix Elements

---
+++ Csharp Advanced/02. Multidimensional Arrays/Lab 01 Sum Matrix Elements/SumMatrixElements.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Lab_01_Sum_Matrix_Elements
+{
+    class SumMatrixElements
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine()
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+
+            int[,] matrix = new int[sizes[0], sizes[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] columnElements = Console.ReadLine()
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = columnElements[col];
+                    sum += columnElements[col];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+
+        }
+    }
+}
