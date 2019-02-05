11bf4bccab1380ec94e89771df25a4a5edc3b0cd
danailstratirv <stratyd@abv.bg>
2019-02-05 22:56:22 +0200

    Group Numbers

---
+++ Csharp Advanced/02. Multidimensional Arrays/Lab Group Numbers/GroupNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Lab_Group_Numbers
+{
+    class GroupNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggedArray = new int[3][];
+
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            jaggedArray[0] = input
+                .Where(x => Math.Abs(x) % 3 == 0)
+                .ToArray();
+
+            jaggedArray[1] = input
+                .Where(x => Math.Abs(x) % 3 == 1)
+                .ToArray();
+
+            jaggedArray[2] = input
+                .Where(x => Math.Abs(x) % 3 == 2)
+                .ToArray();
+
+            foreach (var item in jaggedArray)
+            {
+                Console.WriteLine(string.Join(" ",item));
+            }
+        }
+    }
+}
