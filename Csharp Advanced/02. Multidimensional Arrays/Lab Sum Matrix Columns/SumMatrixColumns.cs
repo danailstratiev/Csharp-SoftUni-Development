22ce6fd8772f9b516977cd591f4a15c544385dcd
danailstratirv <stratyd@abv.bg>
2019-02-05 22:53:45 +0200

    Sum Matrix Columns

---
+++ Csharp Advanced/02. Multidimensional Arrays/Lab Sum Matrix Columns/SumMatrixColumns.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Lab_Sum_Matrix_Columns
+{
+    class SumMatrixColumns
+    {
+        static void Main(string[] args)
+        {
+            var matrixParameters = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            var rows = matrixParameters[0];
+            var cols = matrixParameters[1];
+
+            var calculatingArray = new int[cols];
+            var matrix = new int[rows, cols];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var elements = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = elements[j];
+                    calculatingArray[j] += elements[j];
+                }
+            }
+
+            foreach (var sum in calculatingArray)
+            {
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
