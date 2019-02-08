0a3a686e91cbf646e62d6b86e3748ed4f04add5e
danailstratirv <stratyd@abv.bg>
2019-02-08 23:24:02 +0200

    Matrix Of Palindromes

---
+++ Csharp Advanced/02. Multidimensional Arrays/Exercise 01 Matrix of Palindromes/MatrixOfPalindromes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_01_Matrix_of_Palindromes
+{
+    class MatrixOfPalindromes
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            var parameters = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            var rows = parameters[0];
+            var cols = parameters[1];
+
+            var matrix = new string[rows, cols];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    var sb = new StringBuilder();
+                    sb.Append(alphabet[i]);
+                    sb.Append(alphabet[i + j]);
+                    sb.Append(alphabet[i]);
+
+                    matrix[i, j] = sb.ToString();
+
+                    Console.Write(matrix[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
