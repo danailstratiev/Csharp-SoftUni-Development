74730d14dc0451b22eb5ec54143423a8ea4be0c4
danailstratirv <stratyd@abv.bg>
2019-02-16 23:18:36 +0200

    Copy Binary File

---
+++ Csharp Advanced/03. Streams and Files/Csharp Adv Exercise 04 Copy Binary File/NewCopyBinaryFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+
+namespace Csharp_Adv_Exercise_04_Copy_Binary_File
+{
+    class NewCopyBinaryFile
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new FileStream("../../../copyMe.png", FileMode.Open))
+            {
+                using (var writer = new FileStream("../../../myNewCopy.png", FileMode.Create))
+                {
+                    while (true)
+                    {
+                        byte[] buffer = new byte[4096];
+
+                        int byteSize = reader.Read(buffer, 0, buffer.Length);
+
+                        if (byteSize < 1)
+                        {
+                            break;
+                        }
+
+                        writer.Write(buffer, 0, byteSize);
+                    }
+                }
+            }
+        }
+    }
+}
