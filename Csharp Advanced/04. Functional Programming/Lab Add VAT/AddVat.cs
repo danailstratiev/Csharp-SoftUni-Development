3c16198139b59461081383077648e66efc8b9e30
danailstratirv <stratyd@abv.bg>
2019-02-19 21:58:00 +0200

    Add VAT

---
+++ Csharp Advanced/04. Functional Programming/Lab Add VAT/AddVat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Lab_Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(", ").Select(double.Parse)
+                .Select(n => n * 1.2).
+                ToList();
+
+            foreach (var num in numbers)
+            {
+                Console.WriteLine($"{num:F2}");
+            }
+        }
+    }
+}
