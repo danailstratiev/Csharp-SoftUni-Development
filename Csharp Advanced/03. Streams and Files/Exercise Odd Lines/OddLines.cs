7773d48954d61668559fe2eeecdf00fa2e458d9b
danailstratirv <stratyd@abv.bg>
2019-02-19 21:56:09 +0200

    Odd Lines

---
+++ Csharp Advanced/03. Streams and Files/Exercise Odd Lines/OddLines.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise_01_Odd_Lines
+{
+    class OddLines
+    {
+        static void Main(string[] args)
+        {
+            var path = "C:/Users/ArdesDemo/Desktop/Programming/Projects/Streams and Files/files";
+            var file = "text.txt";
+
+            path = Path.Combine(path, file);
+
+
+            using (var streamReader = new StreamReader(path))
+            {
+                var line = streamReader.ReadLine();
+
+                var counter = 0;
+
+                while (line != null)
+                {
+                    if (counter % 2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    counter++;
+
+                    line = streamReader.ReadLine();
+                }
+            }
+        }
+    }
+}
