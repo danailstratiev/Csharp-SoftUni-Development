24bba3f34866d1c770a91ed90aaffe947788cd7f
danailstratirv <stratyd@abv.bg>
2019-02-19 21:55:10 +0200

    Full Directory Traversal

---
+++ Csharp Advanced/03. Streams and Files/Csharp Adv Exercise 06 Full Directory Traversal/FullDirectoryTraversal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Csharp_Adv_Exercise_06_Full_Directory_Traversal
+{
+    class FullDirectoryTraversal
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+
+            var fileInfoExtensions = new Dictionary<string, List<FileInfo>>();
+
+            foreach (var file in files)
+            {
+                FileInfo info = new FileInfo(file);
+
+                if (fileInfoExtensions.ContainsKey(info.Extension) == false)
+                {
+                    fileInfoExtensions[info.Extension] = new List<FileInfo>() ;
+                }
+
+                fileInfoExtensions[info.Extension].Add(info);
+
+            }
+
+            var pathToDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/Report.txt";
+
+            using (var writer = new StreamWriter(pathToDesktop))
+            {
+                foreach (var kvp in fileInfoExtensions.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+                {
+                    var key = kvp.Key;
+                    var value = kvp.Value;
+
+                    writer.WriteLine(key);
+
+                    foreach (var pair in value.OrderByDescending(x => x.Length))
+                    {
+                        var name = pair.Name;
+                        double size = pair.Length / 1024;
+
+                        writer.WriteLine($"--{name} - {size:F3}kb");
+                    }
+                }
+            }
+        }
+    }
+}
