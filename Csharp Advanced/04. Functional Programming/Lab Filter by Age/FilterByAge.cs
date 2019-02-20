60cb615070d2769255f5dc90ad51572aff41a84c
danailstratirv <stratyd@abv.bg>
2019-02-20 14:50:10 +0200

    Filter By Age

---
+++ Csharp Advanced/04. Functional Programming/Lab Filter by Age/FilterByAge.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Lab_Filter_by_Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var people = new Dictionary<string, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var peopleWithAge = Console.ReadLine().Split(", ").ToArray();
+
+                people.Add(peopleWithAge[0], int.Parse(peopleWithAge[1]));
+            }
+
+            var condition = Console.ReadLine();
+            var ageBoundary = int.Parse(Console.ReadLine());
+            var format = Console.ReadLine();
+
+            people.Where(p => condition == "younger" ? p.Value < ageBoundary : p.Value >= ageBoundary).ToDictionary(x => x.Key, y => y.Value);
+            foreach (var person in people)
+            {
+                switch (format)
+                {
+                    case "name age":
+                        Console.WriteLine($"{person.Key} - {person.Value}");
+                        break;
+                    case "name":
+                        Console.WriteLine($"{person.Key}");
+                        break;
+                    case "age":
+                        Console.WriteLine($"{person.Value}");
+                        break;
+                }
+            }
+        }
+    }
+}
