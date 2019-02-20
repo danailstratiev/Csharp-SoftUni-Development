f44859f0b25f88b3f8363eabe0804ac6bfd83a62
danailstratirv <stratyd@abv.bg>
2019-02-21 01:58:07 +0200

    Applied Arithmetics

---
+++ Csharp Advanced/04. Functional Programming/Csharp Adv Exercise 05 Applied Arithmetics/AppliedArithmetics.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Csharp_Adv_Exercise_05_Applied_Arithmetics
+{
+    class AppliedArithmetics
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            var command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        numbers = numbers.Select(x => x + 1).ToList();
+                        break;
+                    case "multipy":
+                        numbers = numbers.Select(x => x * 2).ToList();
+                        break;
+                    case "subtract":
+                        numbers = numbers.Select(x => x - 1).ToList();
+                        break;
+                    case "print":
+                        Console.WriteLine(string.Join(" ", numbers));
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
