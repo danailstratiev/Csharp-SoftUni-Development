5985f4738e5f106fc5082351a0da4b891bae860e
danailstratirv <stratyd@abv.bg>
2019-02-20 23:37:51 +0200

    Sort Even Numbers

---
+++ Csharp Advanced/04. Functional Programming/Lab Sort Even Numbers/SortEvenNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Lab_Sort_Even_Numbers
+{
+    class SortEvenNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", Console.ReadLine().Split(", ").
+                Select(int.Parse).Where(x => x % 2 == 0).
+                OrderBy(x => x)));
+        }
+    }
+}
