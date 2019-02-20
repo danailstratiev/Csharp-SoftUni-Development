76548d700af82e6912e88e78f8e7215e9fa32da8
danailstratirv <stratyd@abv.bg>
2019-02-20 23:37:04 +0200

    Sum Numbers

---
+++ Csharp Advanced/04. Functional Programming/Lab Sum Numbers/SumNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Lab_Sum_Numbers
+{
+    class SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(", ").Select(Parse).ToArray() ;
+
+            Console.WriteLine(numbers.Count());
+            Console.WriteLine(numbers.Sum());
+        }
+
+        public static Func<string, int> Parse = n => int.Parse(n);
+    }
+}
