aae9f75dc3dd575ef2347bbb8054919263889950
danailstratirv <stratyd@abv.bg>
2019-02-21 01:55:57 +0200

    Knights Of Honor

---
+++ Csharp Advanced/04. Functional Programming/Csharp Adv Exercise 02 Knights of Honor/KnightsOfHonor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Csharp_Adv_Exercise_02_Knights_of_Honor
+{
+    class KnightsOfHonor
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Action<string> beKnighted = k => Console.WriteLine($"Sir {k}");
+
+            foreach (var name in names)
+            {
+                beKnighted(name);
+            }
+        }
+    }
+}
