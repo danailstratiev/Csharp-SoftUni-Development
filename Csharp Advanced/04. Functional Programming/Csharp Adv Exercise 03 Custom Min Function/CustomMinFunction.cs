29ad741dc4c33ffa97e464e19f62ddd021e2b8a7
danailstratirv <stratyd@abv.bg>
2019-02-21 01:56:40 +0200

    Curstom Min Function

---
+++ Csharp Advanced/04. Functional Programming/Csharp Adv Exercise 03 Custom Min Function/CustomMinFunction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Csharp_Adv_Exercise_03_Custom_Min_Function
+{
+    class CustomMinFunction
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            Func<List<int>, int> minNUmber = x => x.Min();
+
+            Console.WriteLine(minNUmber(numbers));
+        }
+    }
+}
