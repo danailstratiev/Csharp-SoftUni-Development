37e4fe7d5050c8b9a6f30e3c21ee6f4f4ca8e042
danailstratirv <stratyd@abv.bg>
2019-02-20 23:38:48 +0200

    Action Point

---
+++ Csharp Advanced/04. Functional Programming/Csharp Adv Exercise 01 Action Point/ActionPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Csharp_Adv_Exercise_01_Action_Point
+{
+    class ActionPoint
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Action<string> printName = name => Console.WriteLine(name);
+
+            foreach (var name in names)
+            {
+                printName(name);
+            }
+        }
+    }
+}
