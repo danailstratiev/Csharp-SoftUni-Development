4bad1429487ceb22eb4ac45ec5efd06c2aeaf016
danailstratirv <stratyd@abv.bg>
2019-02-21 01:57:20 +0200

    Find Evens Or Odds

---
+++ Csharp Advanced/04. Functional Programming/Csharp Adv Exercise 04 Find Evens or Odds/FindEvensOrOdds.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Csharp_Adv_Exercise_04_Find_Evens_or_Odds
+{
+    class FindEvensOrOdds
+    {
+        static void Main(string[] args)
+        {
+            var boundaries = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var startIndex = boundaries[0];
+            var endIndex = boundaries[1];
+
+            var type = Console.ReadLine();
+            var numbers = new HashSet<int>();
+            
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                numbers.Add(i);
+            }
+
+            if (type == "even")
+            {
+                Console.WriteLine(string.Join(" ", numbers.Where(x => x % 2 == 0)));
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", numbers.Where(x => x % 2 != 0)));
+            }
+
+        }
+    }
+}
