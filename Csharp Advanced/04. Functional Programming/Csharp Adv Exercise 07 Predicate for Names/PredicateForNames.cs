8f338bb9295f85c301fa0f4cf06c4eba89cdb776
danailstratirv <stratyd@abv.bg>
2019-02-22 01:18:23 +0200

    Predicate For Names

---
+++ Csharp Advanced/04. Functional Programming/Csharp Adv Exercise 07 Predicate for Names/PredicateForNames.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Csharp_Adv_Exercise_07_Predicate_for_Names
+{
+    class PredicateForNames
+    {
+        static void Main(string[] args)
+        {
+            var allowedNameLength = int.Parse(Console.ReadLine());
+            var validNames = Console.ReadLine().Split().Where(x => x.Length <= allowedNameLength).
+                ToList();
+
+            foreach (var name in validNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
