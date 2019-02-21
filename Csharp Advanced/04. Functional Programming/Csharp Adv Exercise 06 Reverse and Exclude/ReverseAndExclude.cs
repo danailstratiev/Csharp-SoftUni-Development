a575b410f39d327b5cce31e43908168542606de2
danailstratirv <stratyd@abv.bg>
2019-02-22 01:17:42 +0200

    Reverse And Exclude

---
+++ Csharp Advanced/04. Functional Programming/Csharp Adv Exercise 06 Reverse and Exclude/ReverseAndExclude.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Csharp_Adv_Exercise_06_Reverse_and_Exclude
+{
+    class ReverseAndExclude
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var disibleNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(string.Join(" ", numbers.Where(x => x % disibleNumber != 0).Reverse().ToList()));
+        }
+    }
+}
