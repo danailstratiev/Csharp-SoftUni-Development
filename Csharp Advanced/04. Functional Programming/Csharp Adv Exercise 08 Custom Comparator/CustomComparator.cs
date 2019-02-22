8add400241534be4fcbb5dc6fb12370948bb2c23
danailstratirv <stratyd@abv.bg>
2019-02-23 01:07:59 +0200

    Custom Comparator

---
+++ Csharp Advanced/04. Functional Programming/Csharp Adv Exercise 08 Custom Comparator/CustomComparator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Csharp_Adv_Exercise_08_Custom_Comparator
+{
+    class CustomComparator
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            var oddNums = numbers.Where(x => x % 2 != 0).ToList();
+            oddNums.Sort();
+            var evenNums = numbers.Where(x => x % 2 == 0).ToList();
+            evenNums.Sort();
+
+            foreach (var odd in oddNums)
+            {
+                evenNums.Add(odd);
+            }
+            Console.WriteLine(string.Join(" ", evenNums));
+
+        }
+    }
+}
