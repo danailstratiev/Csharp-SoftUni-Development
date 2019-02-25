c3b44756ae9218c145c3c0542d4f16d4a2065499
danailstratirv <stratyd@abv.bg>
2019-02-25 23:51:00 +0200

    Car

---
+++ Csharp Advanced/05. Defining Classes/Lab 01 Car/Car.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Car
+{
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+    }
+}
