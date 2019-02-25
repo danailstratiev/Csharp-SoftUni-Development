e2d1872420f10aa949602aefba101be21e3ad650
danailstratirv <stratyd@abv.bg>
2019-02-25 23:09:09 +0200

    TriFunction

---
+++ Csharp Advanced/04. Functional Programming/Csharp Adv Exercise 13 TriFunction/TriFunction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Csharp_Adv_Exercise_13_TriFunction
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        public int Value { get; set; }
+    }
+    class TriFunction
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Console.ReadLine().Split().
+                FirstOrDefault(x => x.ToCharArray()
+                .Select(y => (int)y)
+                .Sum() >= n));                
+        }
+    }
+}
