84f732b0d1f3c114350f0ee29f5a70c233785a21
danailstratirv <stratyd@abv.bg>
2019-02-27 00:31:04 +0200

    Rectangle Intersection

---
+++ Csharp Advanced/05. Defining Classes/P09_Rectangle_Intersection/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P09_Rectangle_Intersection
+{
+   public class Rectangle
+    {
+        public string ID { get; set; }
+
+        public double Width { get; set; }
+               
+        public double Height { get; set; }
+
+        public double CoordinateX { get; set; }
+
+        public double CoordinateY { get; set; }
+
+        public bool Intersect(Rectangle secondRectangle)
+        {
+            if (this.CoordinateX + this.Width < secondRectangle.CoordinateX ||
+                this.CoordinateY + this.Height < secondRectangle.CoordinateY ||
+                secondRectangle.CoordinateX + secondRectangle.Width < this.CoordinateX ||
+                secondRectangle.CoordinateY + secondRectangle.Height < this.CoordinateY
+                )
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Csharp Advanced/05. Defining Classes/P09_Rectangle_Intersection/RectangleIntersection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P09_Rectangle_Intersection
+{
+   public class RectangleIntersection
+    {
+       public static void Main(string[] args)
+        {
+            var numberOfRectanglesAndIntersections = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var numberOfRectangles = numberOfRectanglesAndIntersections[0];
+            var numberOfIntersections = numberOfRectanglesAndIntersections[1];
+            var allRectangles = new List<Rectangle>();
+
+            for (int i = 0; i < numberOfRectangles; i++)
+            {
+                var properties = Console.ReadLine().Split().ToArray();
+
+                var rectangle = new Rectangle
+                {
+                    ID = properties[0],
+                    Width = double.Parse(properties[1]),
+                    Height =double.Parse(properties[2]),
+                    CoordinateX = double.Parse(properties[3]),
+                    CoordinateY = double.Parse(properties[4])
+                };
+
+                allRectangles.Add(rectangle);
+            }
+
+            for (int i = 0; i < numberOfIntersections; i++)
+            {
+                var rectangleIDs = Console.ReadLine().Split();
+                var firstID = rectangleIDs[0];
+                var secondID = rectangleIDs[1];
+
+                var firstRectangle = allRectangles.FirstOrDefault(x => x.ID == firstID);
+                var secondRectangle = allRectangles.FirstOrDefault(x => x.ID == secondID);
+
+                Console.WriteLine(firstRectangle.Intersect(secondRectangle) ? "true" : "false");
+            }
+
+
+        }
+    }
+}
