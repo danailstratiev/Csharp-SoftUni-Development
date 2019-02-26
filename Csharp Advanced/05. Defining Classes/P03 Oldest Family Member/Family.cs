ab1d6c5578d6363020ba4ed4e9eb2c427d57ee63
danailstratirv <stratyd@abv.bg>
2019-02-26 16:15:28 +0200

    Oldest Family Members

---
+++ Csharp Advanced/05. Defining Classes/P03 Oldest Family Member/Family.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+   public class Family
+    {
+        private List<Person> family = new List<Person>();
+
+        public void AddMember(Person member)
+        {
+            this.family.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.family.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+    }
+}
---
+++ Csharp Advanced/05. Defining Classes/P03 Oldest Family Member/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace DefiningClasses
+{
+   public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                var man = Console.ReadLine().Split().ToArray();
+                var name = man[0];
+                var age = int.Parse(man[1]);
+
+                Person person = new Person(name, age);
+                
+                family.AddMember(person);
+            }
+
+            var oldesMember = family.GetOldestMember();
+
+            Console.WriteLine(oldesMember);
+        }
+    }
+}
