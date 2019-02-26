c9b5b4603d77cc9eec80a167e47682dd8dbab607
danailstratirv <stratyd@abv.bg>
2019-02-26 16:14:44 +0200

    Creating Constructors

---
+++ Csharp Advanced/05. Defining Classes/P02 Creating Constructors/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+   public class Person
+    {
+        private string name;
+
+        private int age;
+
+        public Person()
+            : this ("No name", 1)
+        {
+           
+        }
+
+        public Person(int age)
+        {
+            name = "No name";
+            this.Age = age;
+        }
+
+        public Person(string name, int age)            
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+    }
+}
