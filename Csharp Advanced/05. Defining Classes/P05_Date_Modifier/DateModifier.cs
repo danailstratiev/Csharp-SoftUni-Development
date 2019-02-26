fca4fdeca0b1982e9766a5d21b9b65bb51586fd0
danailstratirv <stratyd@abv.bg>
2019-02-27 00:27:15 +0200

    Date Modifier

---
+++ Csharp Advanced/05. Defining Classes/P05_Date_Modifier/DateModifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace P05_Date_Modifier
+{
+    public class DateModifier
+    {
+        private string firstDate;
+
+        private string secondDate;
+
+        public string FirstDate { get; set; }
+
+        public string SecondDate { get; set; }
+
+        public TimeSpan DateDifference()
+        {
+            this.firstDate = FirstDate;
+            this.secondDate = SecondDate;
+
+            return (DateTime.ParseExact(firstDate, "yyyy MM dd", CultureInfo.InvariantCulture).Date -
+                DateTime.ParseExact(secondDate, "yyyy MM dd", CultureInfo.InvariantCulture).Date);
+        }
+    }
+}
---
+++ Csharp Advanced/05. Defining Classes/P05_Date_Modifier/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using P05_Date_Modifier;
+using System;
+using System.Globalization;
+
+
+namespace DefiningClasses
+{
+   public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+            var firstDate = Console.ReadLine();
+            var secondDate = Console.ReadLine();
+
+            var dateMod = new DateModifier();
+            dateMod.FirstDate = firstDate;
+            dateMod.SecondDate = secondDate;
+
+            var myTime = Math.Abs(dateMod.DateDifference().TotalDays);
+
+            Console.WriteLine(myTime);
+        }
+    }
+}
