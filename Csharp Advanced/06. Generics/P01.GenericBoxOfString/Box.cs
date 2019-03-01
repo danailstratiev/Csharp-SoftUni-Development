3ebc3bf15273a1c8884ac3960eb5d4e0779f9ce2
danailstratirv <stratyd@abv.bg>
2019-03-01 13:41:19 +0200

    Generic Box Of String

---
+++ Csharp Advanced/06. Generics/P01.GenericBoxOfString/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.GenericBoxOfString
+{
+   public class Box <T>
+    {
+        private T item;
+
+        public Box(T item)
+        {
+            this.item = item;
+        }
+
+        public override string ToString()
+        {
+            return $"{item.GetType()}: {item}";         
+        }
+    }
+}
---
+++ Csharp Advanced/06. Generics/P01.GenericBoxOfString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P01.GenericBoxOfString
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentItem = Console.ReadLine();
+                var box = new Box<string>(currentItem);
+
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
