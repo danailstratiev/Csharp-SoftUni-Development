4a6cc9eec33e2c3617d36934870dba33cd6f7922
danailstratirv <stratyd@abv.bg>
2019-03-01 13:40:26 +0200

    Box{T}

---
+++ Csharp Advanced/06. Generics/Lab 01 Box of T/Box{T}.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab_01_Box_of_T
+{
+   public class Box <T>
+    {
+        private List<T> values;
+
+        public Box()
+        {
+            this.values = new List<T>();
+        }
+
+        public void Add (T value)
+        {
+            this.values.Add(value);
+        }
+
+        public T Remove (T value)
+        {
+            var last = values.Last();
+            this.values.RemoveAt(values.Count - 1);
+            return last;
+        }
+
+        public int Count
+        {
+            get { return this.values.Count; }
+        }
+    }
+}
---
+++ Csharp Advanced/06. Generics/Lab 01 Box of T/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Lab_01_Box_of_T
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
