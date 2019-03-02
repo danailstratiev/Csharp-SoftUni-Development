f89f913e699e66abcd8edb830421ae5e4dc027b1
danailstratirv <stratyd@abv.bg>
2019-03-02 04:55:10 +0200

    Generic Swap Method

---
+++ Csharp Advanced/06. Generics/P03.GenericSwapMethodStrings/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03.GenericSwapMethodStrings
+{
+    public class Box<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            this.items = new List<T>();
+        }
+
+        public List<T> Items => this.items;
+
+        public void Add(T item)
+        {
+            this.items.Add(item);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var item in this.items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Csharp Advanced/06. Generics/P03.GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P03.GenericSwapMethodStrings
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine();
+                box.Add(line);
+            }
+
+            var indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var firstIndex = indexes[0];
+            var secondIndex = indexes[1];
+
+            Swap<string>(box.Items, firstIndex, secondIndex);
+
+            Console.WriteLine(box);
+        }
+
+        static void Swap<T>(List<T> currentList, int firstIndex, int secondIndex)
+        {
+            var swap = currentList[firstIndex];
+            currentList[firstIndex] = currentList[secondIndex];
+            currentList[secondIndex] = swap;
+        }
+    }
+}
