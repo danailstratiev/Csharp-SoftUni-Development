40b498d4ab87eb982a283297efbd1457c29ce465
danailstratirv <stratyd@abv.bg>
2019-03-02 04:54:26 +0200

    Generic Box Of Integer

---
+++ Csharp Advanced/06. Generics/P02.2.GenericBoxOfInteger/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02._2.GenericBoxOfInteger
+{
+   public class Box <T>
+    {
+        private T item;
+
+        public Box(T item)
+        {
+            this.item = item;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.item.GetType()}: {item}"; 
+        }
+    }
+}
---
+++ Csharp Advanced/06. Generics/P02.2.GenericBoxOfInteger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P02._2.GenericBoxOfInteger
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                var box = new Box<int>(currentNumber);
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
