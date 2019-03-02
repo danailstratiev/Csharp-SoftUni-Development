69de32b40cfbdfa6f38ab27667040b7d6e723c79
danailstratirv <stratyd@abv.bg>
2019-03-02 04:55:51 +0200

    Generic Swap Method Integers

---
+++ Csharp Advanced/06. Generics/P04.GenericSwapMethodIntegers/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04.GenericSwapMethodIntegers
+{
+    public class Box <T>
+    {
+        public Box()
+        {
+            this.Items = new List<T>();
+        }
+
+        public List<T> Items { get; private set; }
+
+        public void Add(T item)
+        {
+            this.Items.Add(item);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var item in this.Items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Csharp Advanced/06. Generics/P04.GenericSwapMethodIntegers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P04.GenericSwapMethodIntegers
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var box = new Box<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+
+                box.Add(currentNumber);
+            }
+
+            var indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var firstIndex = indexes[0];
+            var secondIndex = indexes[1];
+
+            Swap(box.Items, firstIndex, secondIndex);
+
+        }
+        public static void Swap(List<int> elements, int first, int second)
+        {
+            var value = elements[first];
+            elements[first] = elements[second];
+            elements[second] = value;
+        }
+    }
+}
