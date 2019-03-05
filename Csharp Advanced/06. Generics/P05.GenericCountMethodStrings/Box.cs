165ce31b55e974a605ff669b99e44df45f9283aa
danailstratirv <stratyd@abv.bg>
2019-03-05 04:01:46 +0200

    Generic Count Method Strings

---
+++ Csharp Advanced/06. Generics/P05.GenericCountMethodStrings/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P05.GenericCountMethodStrings
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            this.Items = new List<T>();
+        }
+
+        public List<T> Items { get; private set; }
+
+        public void Add (T item)
+        {
+            this.Items.Add(item);
+        }        
+    }
+}
---
+++ Csharp Advanced/06. Generics/P05.GenericCountMethodStrings/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P05.GenericCountMethodStrings
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var myBox = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine();
+
+                myBox.Add(line);
+            }
+
+            var wantedItem = Console.ReadLine();
+
+            Console.WriteLine(GetCountOfGreaterElements(myBox.Items, wantedItem));
+        }
+
+        public static int GetCountOfGreaterElements<T>(List<T> items, T wantedItem)
+            where T : IComparable
+        {
+            int count = 0;
+
+            foreach (var item in items)
+            {
+                //CompareTo returns -1 , 0 or 1 if the item is smaller, equal ot larger than the wantedItem
+                if (item.CompareTo(wantedItem) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
