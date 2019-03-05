e4b9afbc38f2ef8e6273accd2f3886fe23cec671
danailstratirv <stratyd@abv.bg>
2019-03-05 04:02:24 +0200

    Generic Count Method Doubles

---
+++ Csharp Advanced/06. Generics/P06.GenericCountMethodDoubles/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P06.GenericCountMethodDoubles
+{
+    public class Box <T>
+    {
+        public Box()
+        {
+            this.Items = new List<T>();
+        }
+
+        public List<T> Items { get; private set; }
+
+        public void Add(T item)
+        {
+            Items.Add(item);
+        }
+    }
+}
---
+++ Csharp Advanced/06. Generics/P06.GenericCountMethodDoubles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P06.GenericCountMethodDoubles
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            var myBox = new Box<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var number = double.Parse(Console.ReadLine());
+                myBox.Add(number);
+            }
+
+            var wantedItem = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetCountOfLargerElements(myBox.Items, wantedItem));
+
+        }
+        public static int GetCountOfLargerElements <T> (List<T> items, double wantedItem)
+            where T : IComparable 
+        {
+            var count = 0;
+
+            foreach (var item in items)
+            {
+                if (item.CompareTo(wantedItem) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
