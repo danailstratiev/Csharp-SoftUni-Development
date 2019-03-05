7222e26bc66bc00bd83c73cb30aff26fbb25b8d5
danailstratirv <stratyd@abv.bg>
2019-03-05 04:03:36 +0200

    Threeuple

---
+++ Csharp Advanced/06. Generics/P08.Threeuple/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P08.Threeuple
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                var input = Console.ReadLine().Split(" ").ToArray();
+
+                switch (i)
+                {
+                    case 0:
+                        var name = $"{input[0]} {input[1]}";
+                        var adress = input[2];
+                        var tuple = new Threeuple<string, string, string>(name, adress, input[3]);
+                        Console.WriteLine(tuple);
+                        break;
+                    case 1:
+                        var anotherName = input[0];
+                        var status = input[2];
+                        var isDrunk = status == "drunk" ? true : false ;
+                        var tuple1 = new Threeuple <string, int, bool>(anotherName, int.Parse(input[1]), isDrunk);
+                        Console.WriteLine(tuple1);
+                        break;
+                    case 2:
+                        var someName = input[0];
+                        var accountBalance = double.Parse(input[1]);
+                        var tuple2 = new Threeuple <string,double, string>(someName, accountBalance, input[2]);
+                        Console.WriteLine(tuple2);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Csharp Advanced/06. Generics/P08.Threeuple/Threeuple.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P08.Threeuple
+{
+    public class Threeuple <T, K, W>
+    {
+        private T item1;
+
+        private K item2;
+
+        private W item3;
+
+        public Threeuple(T item1, K item2, W item3)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+            this.item3 = item3;
+        }
+
+        public override string ToString()
+        {
+            return $"{item1} -> {item2} -> {item3}";
+        }
+    }
+}
