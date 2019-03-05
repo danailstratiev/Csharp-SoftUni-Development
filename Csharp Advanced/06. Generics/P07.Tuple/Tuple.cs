5bfeb6ec6489bb6e7f8aab6ffa91aab9ee353fe1
danailstratirv <stratyd@abv.bg>
2019-03-05 04:03:01 +0200

    Tuple

---
+++ Csharp Advanced/06. Generics/P07.Tuple/Tuple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace P07.Tuple
+{
+    public class Tuple <T, K>
+    {
+        private T item1;
+
+        private K item2;
+
+        public Tuple (T item1, K item2)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.item1} -> {this.item2}";
+        }
+
+    }
+}
---
+++ Csharp Advanced/06. Generics/P07.Tuple/TupleExercise.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P07.Tuple
+{
+   public class TupleExercise
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                var input = Console.ReadLine().Split(" ").ToArray();
+
+                switch (i)
+                {
+                    case 0:
+                        var name = $"{input[0]} {input[1]}";
+                        var tuple = new Tuple<string, string>(name, input[2]);
+                        Console.WriteLine(tuple);
+                        break;
+                    case 1:
+                        var anotherName = input[0];
+                        var tuple1 = new Tuple<string, int>(anotherName, int.Parse(input[1]));
+                        Console.WriteLine(tuple1);
+                        break;
+                    case 2:
+                        var someName = int.Parse(input[0]);
+                        var tuple2 = new Tuple<int, double>(someName, double.Parse(input[1]));
+                        Console.WriteLine(tuple2);
+                        break;
+                }
+            }
+        }
+    }
+}
