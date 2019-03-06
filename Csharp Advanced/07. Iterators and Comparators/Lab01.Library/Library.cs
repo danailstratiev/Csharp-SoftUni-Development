9edcca7366e0fd3d8f09704b8f0183a9cb714bab
danailstratirv <stratyd@abv.bg>
2019-03-06 14:58:43 +0200

    Library

---
+++ Csharp Advanced/07. Iterators and Comparators/Lab01.Library/Library.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public void Add (Book book)
+        {
+            this.books.Add(book);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryEnumerator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        => this.GetEnumerator();
+
+        private class LibraryEnumerator : IEnumerator<Book>
+        {
+            private List<Book> books;
+            private int index;
+
+            public LibraryEnumerator(List<Book> books)
+            {
+                this.books = books;
+                this.index = -1;
+            }
+
+            public Book Current
+            {
+                get
+                {
+                    return this.books[index];
+                }
+            }
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {
+            }
+
+            public IEnumerator<Book> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool MoveNext()
+            {
+                this.index++;
+
+                if (this.index < this.books.Count)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            public void Reset()
+            {
+                this.index = -1;
+            }
+        }
+    }
+}
---
+++ Csharp Advanced/07. Iterators and Comparators/Lab01.Library/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace IteratorsAndComparators
+{
+  public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var book1 = new Book("ToshkoAftikanski", 1963, "Angel Karaliichev");
+            var book2 = new Book("The adventures of the little onion", 1951, "Gianni Rodari");
+            var library = new Library(new Book[] { book1, book2 });
+           
+            foreach (var book in library)
+            {
+                Console.WriteLine(book.Title);
+            }
+        }
+    }
+}
