d49f49741cdc8e8fea88eb8867e44497a036515b
danailstratirv <stratyd@abv.bg>
2019-03-08 00:14:07 +0200

    Stack

---
+++ Csharp Advanced/07. Iterators and Comparators/P03.Stack/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P03.Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>();
+
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                if (input.Contains("Push"))
+                {
+                    var elementsToPush = input.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                    for (int i = 1; i < elementsToPush.Length; i++)
+                    {
+                        stack.Push(elementsToPush[i]);
+                    }
+                }
+                else if (input == "Pop")
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("No elements");
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            var helper = new List<string>();
+            var stackCount = stack.Count();
+
+            for (int i = 0; i < stackCount; i++)
+            {
+                var poppedNumber = stack.Pop();
+                helper.Add(poppedNumber);
+                Console.WriteLine(poppedNumber);
+            }
+
+            for (int i = 0; i < stackCount; i++)
+            {
+                Console.WriteLine(helper[i]);
+            }
+
+            
+        }
+    }
+}
---
+++ Csharp Advanced/07. Iterators and Comparators/P03.Stack/Stack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace P03.Stack
+{
+    public class MyStack<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        => this.GetEnumerator();
+    }
+}
