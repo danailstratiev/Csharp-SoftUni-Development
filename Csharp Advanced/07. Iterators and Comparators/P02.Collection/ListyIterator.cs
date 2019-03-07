e87c2bff0f0b3737c3642da3f7e8d7b6fe3c866b
danailstratirv <stratyd@abv.bg>
2019-03-07 23:12:05 +0200

    Collection

---
+++ Csharp Advanced/07. Iterators and Comparators/P02.Collection/ListyIterator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02.Collection
+{
+    public class ListyIterator<T> : IEnumerable <T>
+    {
+        private int index;
+
+        public ListyIterator(List<T> coolList)
+        {
+            this.coolList = coolList;
+            index = 0;
+        }
+
+        public List<T> coolList { get; set; }
+
+
+        public bool Move()
+        {
+            if (index + 1 < coolList.Count)
+            {
+                index++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasNext()
+        {
+            if (index + 1 < coolList.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public T Print()
+        {
+            if (coolList.Count == 0)
+            {
+                throw new IndexOutOfRangeException("Invalid Operation!");
+            }
+            return coolList[index];
+        }
+
+        public string PrintAll()
+        {
+            if (coolList.Count == 0)
+            {
+                throw new IndexOutOfRangeException("Invalid Operation!");
+            }
+
+            return string.Join(" ", coolList);
+        }
+
+        public IEnumerator <T> GetEnumerator()
+        {
+            foreach (var item in this.coolList)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        => this.GetEnumerator();
+
+    }
+}
---
+++ Csharp Advanced/07. Iterators and Comparators/P02.Collection/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P02.Collection
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var listylterator = new ListyIterator<string>(new List<string>());
+
+            while (input != "END")
+            {
+                if (input.Contains("Create"))
+                {
+                    var createList = input.Split().ToArray();
+
+                    if (createList.Length > 1)
+                    {
+                        for (int i = 1; i < createList.Length; i++)
+                        {
+                            listylterator.coolList.Add(createList[i]);
+                        }
+                    }
+                }
+
+                switch (input)
+                {
+                    case "Move":
+                        Console.WriteLine(listylterator.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listylterator.HasNext());
+                        break;
+                    case "Print":
+                        Console.WriteLine(listylterator.Print());
+                        break;
+                    case "PrintAll":
+                        Console.WriteLine(listylterator.PrintAll());
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+
+        public void PrintAll (List<string> someList)
+        {
+            if (someList.Count > 0)
+            {
+                foreach (var item in someList)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+        }
+    }
+}
