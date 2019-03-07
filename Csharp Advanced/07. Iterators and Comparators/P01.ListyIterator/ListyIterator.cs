fab66f5faebb59604e8ade989aef008d32852e4a
danailstratirv <stratyd@abv.bg>
2019-03-07 23:11:15 +0200

    ListyIterator

---
+++ Csharp Advanced/07. Iterators and Comparators/P01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private int index;
+
+        public ListyIterator(List<T> coolList)
+        {
+            this.coolList = coolList;
+            index = 0;
+        }
+
+        public List<T> coolList { get; set; }
+
+
+        public bool Move()
+        {
+            if (index + 1 < coolList.Count)
+            {
+                index++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasNext()
+        {
+            if (index + 1 < coolList.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public T Print()
+        {
+            if (coolList.Count == 0)
+            {
+                throw new IndexOutOfRangeException("Invalid Operation!");
+            }
+            return coolList[index];
+        }
+    }
+}
---
+++ Csharp Advanced/07. Iterators and Comparators/P01.ListyIterator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P01.ListyIterator
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var listylterator = new ListyIterator<string>(new List<string>());
+
+            while (input != "END")
+            {
+                if (input.Contains("Create"))
+                {
+                    var createList = input.Split().ToArray();
+
+                    if (createList.Length > 1)
+                    {
+                        for (int i = 1; i < createList.Length; i++)
+                        {
+                            listylterator.coolList.Add(createList[i]);
+                        }
+                    }                   
+                }
+
+                switch (input)
+                {
+                    case "Move":
+                        Console.WriteLine(listylterator.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listylterator.HasNext());
+                        break;
+                    case "Print":
+                        Console.WriteLine(listylterator.Print());
+                        break;                        
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
