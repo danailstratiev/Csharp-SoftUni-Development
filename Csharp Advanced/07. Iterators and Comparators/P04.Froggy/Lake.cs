a9421c8a9443e54ede437f22acc2852f396c3f2e
danailstratirv <stratyd@abv.bg>
2019-03-10 18:53:42 +0200

    Froggy

---
+++ Csharp Advanced/07. Iterators and Comparators/P04.Froggy/Lake.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04.Froggy
+{
+    public class Lake  : IEnumerable<int>
+    {
+        private List<int> stones;
+
+        public Lake (params int[] stones)
+        {
+            this.stones = stones.ToList();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Count; i+=2)
+            {
+                yield return this.stones[i];
+            }
+
+            var variousStart = stones.Count % 2 == 0
+                ? stones.Count - 1
+                : stones.Count - 2;
+
+            for (int i = variousStart; i >= 0; i-=2)
+            {
+                yield return this.stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        => GetEnumerator();
+    }
+}
---
+++ Csharp Advanced/07. Iterators and Comparators/P04.Froggy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P04.Froggy
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            var jumpNumbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            var lake = new Lake(jumpNumbers);
+                        
+            Console.WriteLine(string.Join(", ", lake));
+            
+        }
+    }
+}
