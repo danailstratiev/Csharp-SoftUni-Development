122a032ff527fecbc3be8c356e0610582a1ad766
danailstratirv <stratyd@abv.bg>
2019-03-10 18:56:12 +0200

    Equality Logic

---
+++ Csharp Advanced/07. Iterators and Comparators/P07.EqualityLogic/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P07.EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public int CompareTo(Person other)
+        {
+            var nameResult = this.Name.CompareTo(other.Name);
+
+            if (nameResult == 0)
+            {
+                return this.Age.CompareTo(other.Age);
+            }
+
+            return nameResult;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Person person)
+            {
+                return this.Name == person.Name && this.Age == person.Age;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() + this.Age.GetHashCode();
+        }
+    }
+}
---
+++ Csharp Advanced/07. Iterators and Comparators/P07.EqualityLogic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P07.EqualityLogic
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new HashSet<Person>();
+            var sortedPeople = new SortedSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split().ToArray();
+
+                var person = new Person(input[0], int.Parse(input[1]));
+
+                people.Add(person);
+                sortedPeople.Add(person);
+            }
+
+            Console.WriteLine(people.Count);
+            Console.WriteLine(sortedPeople.Count);
+        }
+    }
+}
