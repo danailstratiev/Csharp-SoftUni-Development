8c1ab6db321e6385099d684cc0c4c98af8401449
danailstratirv <stratyd@abv.bg>
2019-03-10 18:54:44 +0200

    Comparing Objects

---
+++ Csharp Advanced/07. Iterators and Comparators/P05.ComparingObjects/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P05.ComparingObjects
+{
+    public class Person : IComparable <Person>
+    {
+        private string name;
+
+        private int age;
+
+        private string town;
+
+        public Person(string name, int age, string town )
+        {
+            this.name = name;
+            this.age = age;
+            this.town = town;
+        }
+
+        public int CompareTo(Person other)
+        {
+            if (this.name != other.name)
+            {
+                return -1;
+            }
+            else
+            {
+                if (this.age != other.age)
+                {
+                    return -1;
+                }
+                else
+                {
+                    if (this.town != other.town)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Csharp Advanced/07. Iterators and Comparators/P05.ComparingObjects/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P05.ComparingObjects
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var people = new List<Person>();
+
+            while (input != "END")
+            {
+                var currentPerson = input.Split().ToArray();
+                var person = new Person(currentPerson[0], int.Parse(currentPerson[1]), currentPerson[2]);
+                people.Add(person);
+
+                input = Console.ReadLine();
+            }
+
+            var index = int.Parse(Console.ReadLine());
+
+            if (index >= people.Count)
+            {
+                Console.WriteLine("No matches");
+                return;
+            }
+
+            var equalPeople = 0;
+            var notEqualPeople = 0;
+            var mainMan = people[index];
+
+            for (int i = 0; i < people.Count; i++)
+            {
+                if (people[i].CompareTo(mainMan) == 0)
+                {
+                    equalPeople++;
+                }
+                else
+                {
+                    notEqualPeople++;
+                }
+            }
+
+            if (equalPeople > 1)
+            {
+                Console.WriteLine("No matches");
+
+                return;
+            }
+
+            Console.WriteLine($"{equalPeople} {notEqualPeople} {people.Count}");
+        }
+    }
+}
