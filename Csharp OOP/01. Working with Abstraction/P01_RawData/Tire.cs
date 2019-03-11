1295a30665c4e1f8343338a1ddeb058411b2021b
danailstratirv <stratyd@abv.bg>
2019-03-11 03:29:43 +0200

    Raw Data

---
+++ Csharp OOP/01. Working with Abstraction/P01_RawData/Tire.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_RawData
+{
+    public class Tire
+    {
+        public Tire(double tirePressure, int tireAge)
+        {
+            this.TirePressure = tirePressure;
+            this.TireAge = tireAge;
+        }
+
+        public double TirePressure { get; private set; }
+
+        public int TireAge { get; private set; }
+    }
+}
