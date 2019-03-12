efbee6797843b03c4364fd98b9f75f14fe8d227d
danailstratirv <stratyd@abv.bg>
2019-03-12 19:37:37 +0200

    Jedi Galaxy

---
+++ Csharp OOP/01. Working with Abstraction/P03_JediGalaxy/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_JediGalaxy
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] dimestions = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int x = dimestions[0];
+            int y = dimestions[1];
+
+            int[,] matrix = new int[x, y];
+
+            FillMatrix(matrix, x, y);
+
+            string command = Console.ReadLine();
+            long sum = 0;
+            while (command != "Let the Force be with you")
+            {
+                int[] ivoS = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int[] evil = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int xE = evil[0];
+                int yE = evil[1];
+
+                MoveSith(matrix, xE, yE);
+                
+                int xI = ivoS[0];
+                int yI = ivoS[1];
+
+                sum = MoveJedi(matrix, xI, yI, sum);
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(sum);
+
+        }
+
+        public static void MoveSith(int[,] matrix, int xE, int yE)
+        {
+            while (xE >= 0 && yE >= 0)
+            {
+                if (xE >= 0 && xE < matrix.GetLength(0) && yE >= 0 && yE < matrix.GetLength(1))
+                {
+                    matrix[xE, yE] = 0;
+                }
+                xE--;
+                yE--;
+            }
+        }
+
+        public static long MoveJedi(int[,] matrix, int xI, int yI, long sum)
+        {
+            while (xI >= 0 && yI < matrix.GetLength(1))
+            {
+                if (xI >= 0 && xI < matrix.GetLength(0) && yI >= 0 && yI < matrix.GetLength(1))
+                {
+                    sum += matrix[xI, yI];
+                }
+
+                yI++;
+                xI--;
+            }
+
+            return sum;
+        }
+
+        public static void FillMatrix(int[,] matrix, int x, int y)
+        {
+            int value = 0;
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    matrix[i, j] = value++;
+                }
+            }
+        }
+    }
+}
