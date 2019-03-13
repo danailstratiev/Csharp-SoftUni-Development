58cce56acf49ba1d0749caa112be22a27b4f6b43
danailstratirv <stratyd@abv.bg>
2019-03-14 00:04:01 +0200

    Class Box

---
+++ Csharp OOP/02. Encapsulation/P01.ClassBox/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.ClassBox
+{
+    public class Box
+    {
+        private decimal length;
+        private decimal width;
+        private decimal height;
+
+        public Box (decimal length, decimal width, decimal height)
+        {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+
+        public decimal CalculateLateralSurfaceArea()
+        {
+            return (2 * this.length * this.height) + (2 * this.width * this.height);
+        }
+
+        public decimal CalculateVolume()
+        {
+            return this.length * this.height * this.width;
+        }
+
+        public decimal CalculateSurfaceArea()
+        {
+            return (2 * this.length * this.width) + (2 * this.length * this.height) + (2*this.width * this.height);
+        }
+    }
+}
---
+++ Csharp OOP/02. Encapsulation/P01.ClassBox/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P01.ClassBox
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var length = decimal.Parse(Console.ReadLine());
+            var width = decimal.Parse(Console.ReadLine());
+            var height = decimal.Parse(Console.ReadLine());
+            var box = new Box(length, width, height);
+
+
+            Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():F2}");
+            Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurfaceArea():F2}");
+            Console.WriteLine($"Volume - {box.CalculateVolume():F2}");
+        }
+    }
+}
