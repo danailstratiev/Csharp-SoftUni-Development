3737bfc1804d733cb24c4839e16deb62c437d08e
danailstratirv <stratyd@abv.bg>
2019-03-18 14:41:51 +0200

    Multiple Inheritance

---
+++ Csharp OOP/03. Inheritance/Lab02.MultipleInheritance/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+        }
+    }
+}
+
