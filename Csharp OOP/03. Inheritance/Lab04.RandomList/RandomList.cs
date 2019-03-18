6dfe09935c9094dac201044fba73c2f2201c33e3
danailstratirv <stratyd@abv.bg>
2019-03-18 14:43:52 +0200

    Random List

---
+++ Csharp OOP/03. Inheritance/Lab04.RandomList/RandomList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            this.random = new Random();
+        }
+
+        public string RandomString()
+        {
+            var index = random.Next(0, this.Count);
+            var removedString = this[index];
+            this.RemoveAt(index);
+
+            return removedString;
+        }
+    }
+}
