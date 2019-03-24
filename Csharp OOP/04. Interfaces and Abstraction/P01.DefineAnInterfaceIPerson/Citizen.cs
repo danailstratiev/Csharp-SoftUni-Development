6d052a41e3d2ee6ee8cba4521eaea467b2bd6417
danailstratirv <stratyd@abv.bg>
2019-03-24 03:02:33 +0200

    Define An Interface IPerson

---
+++ Csharp OOP/04. Interfaces and Abstraction/P01.DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,34 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get => this.name;
+
+            private set
+            {
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get => this.age;
+
+            private set
+            {
+                this.age = value;
+            }
+        }
+    }
+}
