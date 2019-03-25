c31f3d26223c35f118cc93aba93d714d42423613
danailstratirv <stratyd@abv.bg>
2019-03-25 16:40:28 +0200

    Border Control

---
+++ Csharp OOP/04. Interfaces and Abstraction/P05.BorderControl/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P05.BorderControl
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var allIds = new List<string>();
+
+            while (input != "End")
+            {
+                var passenger = input.Split(" ").ToArray();
+                allIds.Add(passenger[passenger.Length - 1]);
+
+                input = Console.ReadLine();
+            }
+
+            var fakeId = Console.ReadLine();
+
+            foreach (var id in allIds.Where(x => x.EndsWith(fakeId)))
+            {
+                Console.WriteLine(id);
+            }
+        }
+    }
+}
