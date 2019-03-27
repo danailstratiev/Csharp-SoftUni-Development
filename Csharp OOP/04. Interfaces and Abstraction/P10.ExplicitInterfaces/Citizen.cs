104a1ec30d1d0cd58c23585eb1c75f34022673e9
danailstratirv <stratyd@abv.bg>
2019-03-27 17:35:34 +0200

    Explicit Interfaces

---
+++ Csharp OOP/04. Interfaces and Abstraction/P10.ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10.ExplicitInterfaces
+{
+    public class Citizen : IPerson, IResident
+    {
+        public Citizen(string name, int age, string country)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Country = country;
+        }
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string Country { get; private set; }
+
+        public string GetName()
+        {
+            return "Mr/Ms/Mrs";
+        }
+
+        public string GetName(string name)
+        {
+            return name;
+        }
+
+        public override string ToString()
+        {
+            return this.Name + Environment.NewLine + GetName() + " " + GetName(this.Name);
+        }
+    }
+}
---
+++ Csharp OOP/04. Interfaces and Abstraction/P10.ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P10.ExplicitInterfaces
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                var personInfo = input.Split();
+                var name = personInfo[0];
+                var country = personInfo[1];
+                var age = int.Parse(personInfo[2]);
+
+                var citizen = new Citizen(name, age, country);
+
+                Console.WriteLine(citizen);
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
