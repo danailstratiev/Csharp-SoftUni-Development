8fd1917640e4682a5de3023cb9e31c5faf997150
danailstratirv <stratyd@abv.bg>
2019-03-27 18:19:21 +0200

    Cars

---
+++ Csharp OOP/04. Interfaces and Abstraction/Cars/Tesla.cs
@@ -0,0 +1,37 @@
+﻿namespace Cars
+{
+    using System;
+
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int bateries)
+        {
+           this.Model = model;
+           this.Color = color;
+           this.Bateries = bateries;
+        }
+
+        public string Model {get; private set;}
+
+        public string Color {get; private set;}
+
+        public int Bateries { get; private set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} {this.GetType().Name} {this.Model} with {this.Bateries} Batteries" + Environment.NewLine +
+                this.Start() + Environment.NewLine +
+                this.Stop();
+        }
+    }
+}
