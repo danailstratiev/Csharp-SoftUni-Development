5fee1c013549361efdc3c2f00ef294b551e82dcd
danailstratirv <stratyd@abv.bg>
2019-03-28 23:43:05 +0200

    Animals

---
+++ Csharp OOP/05. Polymorphism/Lab02.Animals/Animal.cs
@@ -0,0 +1,21 @@
+﻿namespace Animals
+{
+    using System;
+
+    public class Animal
+    {
+        protected Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+
+        public string Name { get; protected set; }
+        public string FavouriteFood { get ; protected set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.FavouriteFood}";
+        }
+    }
+}
