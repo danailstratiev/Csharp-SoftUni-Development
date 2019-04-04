71fbe887669e5173c3cd874dd5732f1ccf9aa766
danailstratirv <stratyd@abv.bg>
2019-04-04 19:31:10 +0300

    Black Box Integer

---
+++ Csharp OOP/07. Reflection and Attributes/P02_BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,36 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            var type = typeof(BlackBoxInteger);
+
+            var box = (BlackBoxInteger)Activator.CreateInstance(type, true);
+
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                var command = input.Split("_")[0];
+                var number = int.Parse(input.Split("_")[1]);
+
+                var method = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+                    .First(x => x.Name == command);
+
+                method.Invoke(box, new object[] { number });
+
+                var fieldValue = type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
+                    .First(x => x.Name == "innerValue").GetValue(box);
+
+                Console.WriteLine(fieldValue);
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
