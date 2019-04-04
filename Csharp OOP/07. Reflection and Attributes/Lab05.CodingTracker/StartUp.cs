d80c16cae5c56f0dd59857995ae7cb946bc8ac37
danailstratirv <stratyd@abv.bg>
2019-04-04 19:27:32 +0300

    Coding Tracker

---
+++ Csharp OOP/07. Reflection and Attributes/Lab05.CodingTracker/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+[Author("Stewie")]
+public class StartUp
+{
+    [Author("Bryan")]
+    public static void Main(string[] args)
+    {
+        var tracker = new Tracker();
+    } 
+}
+
---
+++ Csharp OOP/07. Reflection and Attributes/Lab05.CodingTracker/Tracker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+
+public class Tracker
+{
+    public static void PrintMethodsByAuthor()
+    {
+        var type = typeof(StartUp);
+        var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+
+        foreach (var method in methods)
+        {
+            if (method.CustomAttributes.Any(x => x.AttributeType == typeof(AuthorAttribute)))
+            {
+                var attributes = method.GetCustomAttributes(false);
+
+                foreach (AuthorAttribute item in attributes)
+                {
+                    Console.WriteLine($"{method.Name} is written by {item.Name}");
+                }
+            }
+        }
+    }
+}
+
