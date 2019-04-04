ed4bd9652c59a20ebdda36d92d8b0affa1e22719
danailstratirv <stratyd@abv.bg>
2019-04-04 19:29:51 +0300

    Harvesting Fields

---
+++ Csharp OOP/07. Reflection and Attributes/P01_HarvestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,46 @@
+﻿ namespace P01_HarvestingFields
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            Type type = typeof(HarvestingFields);
+
+            FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic |
+                BindingFlags.Instance);
+
+            var input = Console.ReadLine();
+
+            while (input != "HARVEST")
+            {
+                if (input != "all")
+                {
+                    var fieldsToPrint = fields.
+                        Where(x => x.Attributes.ToString().ToLower().Replace("family", "protected") == input).
+                        ToArray();
+
+                    foreach (var field in fieldsToPrint)
+                    {
+                        var fieldModifier = field.Attributes.ToString().ToLower().Replace("family", "protected");
+                        Console.WriteLine($"{fieldModifier} {field.FieldType.Name} {field.Name}");
+                    }
+                }
+                else
+                {
+                    foreach (var field in fields)
+                    {
+                        var fieldModifier = field.Attributes.ToString().ToLower().Replace("family", "protected");
+                        Console.WriteLine($"{fieldModifier} {field.FieldType.Name} {field.Name}");
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+        }       
+    }
+}
