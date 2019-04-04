dcb3ff811750a211ca56c6155b0260e0cd460938
danailstratirv <stratyd@abv.bg>
2019-04-04 19:24:59 +0300

    Collector

---
+++ Csharp OOP/07. Reflection and Attributes/Lab04.Collector/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+
+public class Spy
+{
+    public string CollectGettersAndSetters(string className)
+    {
+        var type = Type.GetType(className);
+        var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Public |
+            BindingFlags.Instance | BindingFlags.Static);
+        var sb = new StringBuilder();
+
+        foreach (var method in methods.Where(x => x.Name.Contains("get")))
+        {
+            sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+        }
+
+        foreach (var method in methods.Where(x => x.Name.Contains("set")))
+        {
+            sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+        }
+        return sb.ToString().Trim();
+    }
+}
+
---
+++ Csharp OOP/07. Reflection and Attributes/Lab04.Collector/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        Spy spy = new Spy();
+        string result = spy.CollectGettersAndSetters("Hacker");
+        Console.WriteLine(result);
+    }
+}
+
