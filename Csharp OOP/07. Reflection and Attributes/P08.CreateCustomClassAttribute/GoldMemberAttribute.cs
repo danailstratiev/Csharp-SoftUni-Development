3e493db1ad6c0be188b9707ddf5299c8e296199e
danailstratirv <stratyd@abv.bg>
2019-04-07 04:13:04 +0300

    Create Custom Class Attribute

---
+++ Csharp OOP/07. Reflection and Attributes/P08.CreateCustomClassAttribute/GoldMemberAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P08.CreateCustomClassAttribute
+{
+    public class GoldMemberAttribute : Attribute
+    {
+        private const string author = "Pesho";
+        private const int revision = 3;
+        private const string description = "Used for C# OOP Advanced Course - Enumerations and Attributes.";
+        private const string reviewers = "Pesho, Svetlio";
+
+        public GoldMemberAttribute()
+        {
+            Author = author;
+            Revision = revision;
+            Description = description;
+            Reviewers = reviewers;
+        }
+
+        public string Author { get; }
+
+        public int Revision { get; }
+
+        public string Description {get;}
+
+        public string Reviewers { get; }
+    }
+}
---
+++ Csharp OOP/07. Reflection and Attributes/P08.CreateCustomClassAttribute/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace P08.CreateCustomClassAttribute
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            var type = typeof(GoldMemberAttribute);
+            var goldMember = (GoldMemberAttribute)Activator.CreateInstance(type);
+
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                var currentInfo = type.GetProperties(BindingFlags.Public | BindingFlags.Instance |
+                    BindingFlags.Static).
+                    First(x => x.Name == input);
+
+                if (input != "Description")
+                {
+                    Console.WriteLine($"{currentInfo.Name}: {currentInfo.GetValue(goldMember)}");
+                }
+                else
+                {
+                    Console.WriteLine($"Class description: {currentInfo.GetValue(goldMember)}");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
