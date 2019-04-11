fbd11cdaa032c34c2b9ad8b440679446eb35ea63
danailstratirv <stratyd@abv.bg>
2019-04-11 21:47:39 +0300

    Database

---
+++ Csharp OOP/09. Unit Testing/P01.Database/Database.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Database
+{
+    public class Database
+    {
+        private const int MaxArraySize = 16;
+        private int[] numbers;
+        private int index;
+
+        public Database()
+        {
+            this.numbers = new int[MaxArraySize];
+            this.index = 0;
+        }
+
+        public Database(int[] integers)
+        {
+            if (integers.Length != MaxArraySize)
+            {
+                throw new InvalidOperationException($"Array length must be exactly {MaxArraySize}!");
+            }
+
+            this.numbers = integers;
+            this.index = integers.Length - 1;
+        }
+
+        public void Add (int value)
+        {
+            if (this.index < value)
+            {
+                this.numbers[++this.index] = value;
+            }
+
+            throw new InvalidOperationException("Database is full!");
+        }
+
+        public void Remove()
+        {
+            if (this.index == 0)
+            {
+                throw new InvalidOperationException("Database is empty!");
+            }
+
+            this.numbers[index] = 0;
+            this.index--;
+        }
+
+        public int[] Fetch()
+        {
+           return this.numbers.Take(this.index + 1).ToArray();
+        }
+    }
+}
---
+++ Csharp OOP/09. Unit Testing/P01.Database/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01.Database
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
