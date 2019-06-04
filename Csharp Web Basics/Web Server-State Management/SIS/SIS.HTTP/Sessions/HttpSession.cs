1e572515ee278f02a4fc5e4ce9993b54d6268f0a
danailstratirv <stratyd@abv.bg>
2019-06-04 18:56:14 +0300

    .

---
+++ Csharp Web Basics/Web Server-State Management/SIS/SIS.HTTP/Sessions/HttpSession.cs
@@ -0,0 +1,46 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.HTTP.Sessions
+{
+    public class HttpSession : IHttpSession
+    {
+        private readonly Dictionary<string, object> sessionParameters;
+        public HttpSession(string id)
+        {
+            this.Id = id;
+            this.sessionParameters = new Dictionary<string, object>();
+        }
+
+        public string Id { get; } 
+
+        public void AddParameter(string name, object parameter)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(name, nameof(name));
+            CoreValidator.ThrowIfNull(parameter, nameof(parameter));
+
+            this.sessionParameters[name] = parameter;
+        }
+
+        public void ClearParameters()
+        {
+            this.sessionParameters.Clear();
+        }
+
+        public bool ContainsParameter(string name)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(name, nameof(name));
+
+            return this.sessionParameters.ContainsKey(name);
+        }
+
+        public object GetParameter(string name)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(name, nameof(name));
+
+            return this.sessionParameters[name];
+        }
+    }
+}
---
+++ Csharp Web Basics/Web Server-State Management/SIS/SIS.HTTP/Sessions/IHttpSession.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.HTTP.Sessions
+{
+   public interface IHttpSession
+    {
+        string Id { get; }
+
+        object GetParameter(string name);
+
+        bool ContainsParameter(string name);
+
+        void AddParameter(string name, object parameter);
+
+        void ClearParameters(); 
+    }
+}
