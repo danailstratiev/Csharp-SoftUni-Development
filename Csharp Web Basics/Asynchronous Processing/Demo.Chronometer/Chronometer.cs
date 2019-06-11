941bd1cdab9cb71308da8f089fc7da7622942345
danailstratirv <stratyd@abv.bg>
2019-06-11 23:16:33 +0300

    Demo Chronometer

---
+++ Csharp Web Basics/Asynchronous Processing/Demo.Chronometer/Chronometer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demo.Chronometer
+{
+    public class Chronometer : IChronometer
+    {
+        private long miliseconds;
+
+        private bool isRunning;
+
+        public Chronometer()
+        {
+            this.Reset();
+        }
+
+        public string GetTime => $"{miliseconds/60000:D2}:{miliseconds/1000:D2}:{miliseconds % 1000:D4}";
+
+        public List<string> Laps { get; private set; }
+
+        public void Start()
+        {
+            this.isRunning = true;
+
+            //Task.Run indices that the process will run on another thread of the CPU
+            Task.Run(() =>
+            {
+                while (this.isRunning)
+                {
+                    Thread.Sleep(1);
+                    this.miliseconds++;
+                }
+            });
+        }
+
+        public string Lap()
+        {
+            var lap = this.GetTime;
+            this.Laps.Add(lap);
+            return lap;
+        }
+
+        public void Reset()
+        {
+            this.Stop();
+            this.miliseconds = 0;
+            this.Laps = new List<string>();
+        }        
+
+        public void Stop()
+        {
+            this.isRunning = false;
+        }
+    }
+}
---
+++ Csharp Web Basics/Asynchronous Processing/Demo.Chronometer/IChronometer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Chronometer
+{
+   public interface IChronometer
+    {
+        string GetTime { get; }
+
+        List<string> Laps { get; }
+
+        void Start();
+
+        void Stop();
+
+        string Lap();
+
+        void Reset();
+    }
+}
