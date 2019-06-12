2e06c250b29462e25293ac06f5ecb23a6eb13641
danailstratirv <stratyd@abv.bg>
2019-06-12 23:33:00 +0300

    Simple Http Server

---
+++ Csharp Web Basics/Asynchronous Processing/Demo.Chronometer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Demo.Chronometer
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var chronometer = new Chronometer();
+
+            var input = Console.ReadLine();
+
+            while (input != "exit")
+            {
+                switch (input)
+                {
+                    case "start":
+                        chronometer.Start();
+                        break;
+                    case "stop":
+                        chronometer.Stop();
+                        break;
+                    case "lap":
+                        Console.WriteLine(chronometer.Lap());
+                        break;
+                    case "reset":
+                        chronometer.Reset();
+                        break;
+                    case "time":
+                        Console.WriteLine(chronometer.GetTime);
+                        break;
+                    case "laps":
+                        Console.WriteLine("Laps: " + (chronometer.Laps.Count == 0 
+                            ? "no laps."
+                            : "\r\n" + string.Join("\r\n", chronometer
+                            .Laps
+                            .Select((lap, index) => $"{index}. {lap}"))));
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Csharp Web Basics/SoftUniHttpServer/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace SoftUniHttpServer
+{
+    class Program
+    {
+        const string NewLine = "\r\n";
+        static void Main(string[] args)
+        {
+            var tcpListener = new TcpListener(IPAddress.Loopback, 12345);
+
+            tcpListener.Start();
+
+            while (true)
+            {
+                var client = tcpListener.AcceptTcpClient();
+                // WebUtility.Decode
+                Task.Run(() => ProcessClient(client));
+            }
+        }
+
+        public static async Task ProcessClient(TcpClient client)
+        {
+            using (var stream = client.GetStream())
+            {
+                var requestBytes = new byte[100000];
+                var readBytes = await stream.ReadAsync(requestBytes, 0, requestBytes.Length);
+                var stringRequest = Encoding.UTF8.GetString(requestBytes, 0, readBytes);
+                Console.WriteLine(new string('=', 70));
+                Console.WriteLine(stringRequest);
+
+                var responseBody = DateTime.Now.ToString();
+                var response = "HTTP/1.0 200 OK" + NewLine + 
+                    "Content type: text/html" + NewLine +
+                    "Set-Cookie: cookie1=test; Domain=.localhost" + NewLine +
+                    "Server: MyCustomServer 1.0" + NewLine +
+                    $"Content-Length {responseBody}";
+                var responceBytes = Encoding.UTF8.GetBytes(response);
+                stream.Write(responceBytes, 0, responceBytes.Length);
+            }
+        }
+    }
+}
