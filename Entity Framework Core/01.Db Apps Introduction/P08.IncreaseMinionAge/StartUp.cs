e997369cacc28050f23979ad23565b4a5e20f748
danailstratirv <stratyd@abv.bg>
2019-10-29 00:32:12 +0200

    Increase Minion Age

---
+++ Entity Framework Core/01.Db Apps Introduction/P08.IncreaseMinionAge/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using P01.InitialSetup;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace P08.IncreaseMinionAge
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var ids = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            using (SqlConnection connection = new SqlConnection(Configuration.ConnectionString))
+            {
+                connection.Open();
+
+                string updateMinions = @"UPDATE Minions
+   SET Name = UPPER(LEFT(Name, 1)) + SUBSTRING(Name, 2, LEN(Name)), Age += 1
+ WHERE Id = @Id";
+
+                for (int i = 0; i < ids.Count; i++)
+                {
+                    using (SqlCommand command = new SqlCommand(updateMinions, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", ids[i]);
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                string minionsQuery = "SELECT Name, Age FROM Minions";
+
+                using (SqlCommand command = new SqlCommand(minionsQuery, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader[0]} {reader[1]}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
