83acb8759a08a5091d6f4f25c322648fcd5a7870
danailstratirv <stratyd@abv.bg>
2019-11-21 05:15:31 +0200

    Auto Mapping Objects

---
+++ Entity Framework Core/07.Auto Mapping Objects/FastFood.Web/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,40 @@
+﻿namespace FastFood.Web.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Web.ViewModels.Categories;
+    using FastFood.Web.ViewModels.Employees;
+    using FastFood.Web.ViewModels.Items;
+    using Models;
+
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            this.CreateMap<Position, RegisterEmployeeViewModel>()
+                .ForMember(x => x.PositionId, y => y.MapFrom(x => x.Id));
+
+            this.CreateMap<RegisterEmployeeInputModel, Employee>();
+
+            this.CreateMap<Employee, EmployeesAllViewModel>()
+                .ForMember(x => x.Position, y => y.MapFrom(x =>
+                x.Position.Name));
+
+            this.CreateMap<CreateCategoryInputModel, Category>().ForMember(x => x.Name,
+                y => y.MapFrom(x => x.CategoryName));
+
+            this.CreateMap<Category, CategoryAllViewModel>();
+
+            this.CreateMap<Category, CreateItemViewModel>().ForMember(x => x.CategoryId,
+                y => y.MapFrom(x => x.Id));
+        }
+    }
+}
---
+++ Entity Framework Core/07.Auto Mapping Objects/FastFood.Web/ViewModels/Employees/RegisterEmployeeInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Web.ViewModels.Employees
+{
+    public class RegisterEmployeeInputModel
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(16, 65)]
+        public int Age { get; set; }
+
+        public int PositionId { get; set; }
+
+        public string PositionName { get; set; }
+
+        public string Address { get; set; }
+    }
+}
