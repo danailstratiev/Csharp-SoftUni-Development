60c1a252b09c0c5358a3250dac53087e659b1a7a
danailstratirv <stratyd@abv.bg>
2020-11-10 00:01:31 +0200

    Data Export with Json done.

--- Entity Framework Core/11.Exam Preparations/Exam preparation 2/VaporStore/DataProcessor/Serializer.cs
+++ Entity Framework Core/11.Exam Preparations/Exam preparation 2/VaporStore/DataProcessor/Serializer.cs
@@ -1,13 +1,41 @@
 ﻿namespace VaporStore.DataProcessor
 {
 	using System;
-	using Data;
+    using System.Linq;
+    using Data;
+    using Newtonsoft.Json;
 
-	public static class Serializer
+    public static class Serializer
 	{
 		public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
 		{
-			throw new NotImplementedException();
+            var genres = context.Genres.Where(g => genreNames.Contains(g.Name))
+                                       .Select(x => new
+                                       {
+                                           Id = x.Id,
+                                           Genre = x.Name,
+                                           Games = x.Games
+                                           .Where(p => p.Purchases.Any())
+                                           .Select(g => new
+                                           {
+                                               Id = g.Id,
+                                               Title = g.Name,
+                                               Developer = g.Developer.Name,
+                                               Tags = string.Join(", ", g.GameTags.Select(t => t.Tag.Name).ToArray()),
+                                               Players = g.Purchases.Count
+                                           })
+                                           .OrderByDescending(p => p.Players)
+                                           .ThenBy(g => g.Id)
+                                           .ToArray(),
+                                           TotalPlayers = x.Games.Sum(p => p.Purchases.Count)
+                                       })
+                                       .OrderByDescending(t => t.TotalPlayers)
+                                       .ThenBy(g => g.Id)
+                                       .ToArray();
+
+            var jsonResult = JsonConvert.SerializeObject(genres, Formatting.Indented);
+
+            return jsonResult;
 		}
 
 		public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)
